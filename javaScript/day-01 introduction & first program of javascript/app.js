554b3506e5e0ddbc2e58ae77a669b7edb8c4f727
Muntazir Hussain <muntazirhussainpanhwar@gmail.com>
2026-07-30 22:08:36 +0500

    Day 01: Learn JavaScript introduction and practice first JavaScript code

---
+++ javaScript/day-01 introduction & first program of javascript/app.js
@@ -0,0 +1,68 @@
+// =======================================
+// JavaScript Day 1 Practice
+// =======================================
+
+// Single Line Comment
+
+/*
+   Multi 
+   Line
+   Comment
+*/
+
+// console.log()
+
+console.log("Welcome to JavaScript!");
+
+console.log("My name is Muntazir Hussain.");
+
+console.log("Today is my first day of JavaScript.");
+
+
+// console.info()
+
+console.info("This is an information message.");
+
+
+// console.warn()
+
+console.warn("This is a warning message.");
+
+
+// console.error()
+
+console.error("This is an error message.");
+
+
+// console.clear()
+
+// Uncomment the line below to clear the console.
+// console.clear();
+
+
+// console.time() and console.timeEnd()
+
+console.time("Loading");
+
+// Some simple statements
+
+console.log("Learning JavaScript...");
+console.log("Practicing Console Methods...");
+console.log("Exploring Chrome DevTools...");
+
+console.timeEnd("Loading");
+
+
+// console.group()
+
+console.group("Introduction");
+
+console.log("Name: Muntazir Hussain");
+
+console.log("Course: JavaScript");
+
+console.log("Day: 1");
+
+console.groupEnd();
+
+console.log("Day 1 Practice Completed!");
